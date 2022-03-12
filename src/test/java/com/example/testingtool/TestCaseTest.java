ac6bea89189a8f54a05ba9adecbecfb347df01ff
Tim Mortimer <tim@kiteframe.co.uk>
2022-03-12 18:04:00 +0000

    Instantiate the test case under test in setUp()

--- src/test/java/com/example/testingtool/TestCaseTest.java
+++ src/test/java/com/example/testingtool/TestCaseTest.java
@@ -1,19 +1,23 @@
 package com.example.testingtool;
 
 public class TestCaseTest extends TestCase {
+    private WasRun test;
 
     public TestCaseTest(String name) {
         super(name);
     }
 
+    @Override
+    protected void setUp() {
+        test = new WasRun("testMethod");
+    }
+
     public void testRunning() throws Exception {
-        var test = new WasRun("testMethod");
         test.run(WasRun.class);
         if (!test.wasRun) throw new Exception();
     }
 
     public void testSetUp() throws Exception {
-        var test = new WasRun("testMethod");
         test.run(WasRun.class);
         if (!test.wasSetUp) throw new Exception();
     }
