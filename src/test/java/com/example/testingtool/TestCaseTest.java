241cb669fccdab2ccc7bac4ef9c027dc7f1c4daf
Tim Mortimer <tim@kiteframe.co.uk>
2022-03-12 16:44:48 +0000

    Switch to using exceptions for test failures

--- src/test/java/com/example/testingtool/TestCaseTest.java
+++ src/test/java/com/example/testingtool/TestCaseTest.java
@@ -8,9 +8,9 @@ public class TestCaseTest extends TestCase {
 
     public void testRunning() throws Exception {
         var test = new WasRun("testMethod");
-        assert !test.wasRun;
+        if (test.wasRun) throw new Exception();
         test.run(WasRun.class);
-        assert test.wasRun;
+        if (!test.wasRun) throw new Exception();
     }
 
     public static void main(String[] args) throws Exception {
