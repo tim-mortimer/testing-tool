a2b98cfe44e84ce99469510f2795ed8470933bd6
Tim Mortimer <tim@kiteframe.co.uk>
2022-03-13 08:16:07 +0000

    Remove redundant setUp method

--- src/test/java/com/example/testingtool/TestCaseTest.java
+++ src/test/java/com/example/testingtool/TestCaseTest.java
@@ -1,18 +1,12 @@
 package com.example.testingtool;
 
 public class TestCaseTest extends TestCase {
-    private WasRun test;
-
     public TestCaseTest(String name) {
         super(name);
     }
 
-    @Override
-    protected void setUp() {
-        test = new WasRun("testMethod");
-    }
-
     public void testTemplateMethod() {
+        var test = new WasRun("testMethod");
         test.run(WasRun.class);
         assertTrue(test.log.equals("setUp testMethod "));
     }
